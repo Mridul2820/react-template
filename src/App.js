0de2ef65f0e6fb3e658a6fd5a2aaa3db7e13dbb7
Mridul2820 <immridulpanda2820@gmail.com>
2022-05-21 11:00:20 +0530

    prettier add

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from 'react';
 
 const App = () => {
-  return (
-    <div className='flex justify-center items-center'>
-      React Template
-    </div>
-  )
-}
+  return <div className="flex justify-center items-center">React Template</div>;
+};
 
-export default App
+export default App;
